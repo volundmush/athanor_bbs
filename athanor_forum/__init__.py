2f9fd0869be34f55a208b100838062875c8c7ef1
Andrew Bastien <volundmush@gmail.com>
2020-01-15 06:37:29 -0500

    Splitting Athanor into plugins and creating initial plugins!

--- athanor_forum/__init__.py
+++ athanor_forum/__init__.py
@@ -3,7 +3,7 @@ INSTALLED_APPS = ['athanor_forum']
 GLOBAL_SCRIPTS = dict()
 
 GLOBAL_SCRIPTS['forum'] = {
-    'typeclass': 'athanor.controllers.forum.AthanorForumController',
+    'typeclass': 'athanor_forum.controllers.AthanorForumController',
     'repeats': -1, 'interval': 60, 'desc': 'Forum BBS API',
     'locks': "admin:perm(Admin)",
 }
