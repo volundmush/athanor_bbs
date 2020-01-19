80a3050299b3760cd293b12a69efebb441e80d92
Andrew Bastien <volundmush@gmail.com>
2020-01-19 12:32:42 -0500

    Tidying up Athanor and beginning work on athanor_channels

--- athanor_forum/__init__.py
+++ athanor_forum/__init__.py
@@ -1,7 +1,15 @@
+from collections import defaultdict
+
+
 def load(settings):
     settings.INSTALLED_APPS.extend(["athanor_forum"])
     settings.GLOBAL_SCRIPTS['forum'] = {
         'typeclass': 'athanor_forum.controllers.AthanorForumController',
         'repeats': -1, 'interval': 60, 'desc': 'Forum BBS API',
         'locks': "admin:perm(Admin)",
+    }
+    settings.CHANNEL_SYSTEMS = defaultdict(dict)
+
+    settings.CHANNEL_SYSTEMS['account'] = {
+        'channel_typeclass': 'athanor_channels.gamedb.AthannorAccountChannel'
     }
