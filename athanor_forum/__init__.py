12e8e752ede463a8a68e2b80bcab728b271a6639
Andrew Bastien <volundmush@gmail.com>
2020-01-29 17:57:04 -0500

    Fixing up account forum

--- athanor_forum/__init__.py
+++ athanor_forum/__init__.py
@@ -3,8 +3,6 @@ from collections import defaultdict
 
 def init_settings(settings):
     settings.INSTALLED_APPS.append("athanor_forum")
-    settings.GLOBAL_SCRIPTS['forum'] = {
-        'typeclass': 'athanor_forum.controllers.AthanorForumController',
-        'repeats': -1, 'interval': 60, 'desc': 'Forum BBS API',
-        'locks': "admin:perm(Admin)",
+    settings.CONTROLLERS['forum'] = {
+        'class': 'athanor_forum.controllers.AthanorForumController'
     }
